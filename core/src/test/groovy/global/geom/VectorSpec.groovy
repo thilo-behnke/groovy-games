00578e9c5ea164a8ab1650c1a7a069357061e276
Thilo Behnke <thilo.behnke@gmx.net>
2020-07-19 11:41:36 +0200

    add vector subtraction test

--- core/src/test/groovy/global/geom/VectorSpec.groovy
+++ core/src/test/groovy/global/geom/VectorSpec.groovy
@@ -9,8 +9,16 @@ class VectorSpec extends Specification {
         expect:
         v1 + v2 == vRes
         where:
-        v1 | v2 | vRes
+        v1                                  | v2                                    | vRes
         new Vector(x: 10.2349, y: 49.23049) | new Vector(x: 23.230945, y: 49.29348) | new Vector(x: 33.465845, y: 98.52397)
+    }
+
+    def 'vector subtraction'() {
+        expect:
+        v1 + v2 == vRes
+        where:
+        v1                                  | v2                                    | vRes
+        new Vector(x: 96.29032, y: 233.203) | new Vector(x: 45.5095, y: 994.223222) | new Vector(x: 141.79982, y: 1227.426222)
     }
 
     def 'vector multiplication'() {
