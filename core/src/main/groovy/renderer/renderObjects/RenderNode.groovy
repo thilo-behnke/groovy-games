e34fad81d1458b94ec27c6a338cf51da6bf4d337
Thilo Behnke <thilo.behnke@gmx.net>
2020-07-20 20:22:31 +0200

    allow empty renderNode.renderObjects

--- core/src/main/groovy/renderer/renderObjects/RenderNode.groovy
+++ core/src/main/groovy/renderer/renderObjects/RenderNode.groovy
@@ -4,15 +4,15 @@ import renderer.options.RenderOptions
 
 class RenderNode {
     // TODO: Should be optional, in this case the node has only structural purpose. Maybe add a string descriptor field?
-    Renderable renderObject
-    RenderOptions renderOptions
+    Optional<Renderable> renderObject
+    Optional<RenderOptions> renderOptions
     List<RenderNode> childNodes = []
 
-    static RenderNode node(Renderable obj, RenderOptions options, List<RenderNode> childNodes) {
-        new RenderNode(renderObject: obj, renderOptions: options, childNodes: childNodes)
+    static RenderNode node(List<RenderNode> childNodes, Renderable obj = null, RenderOptions options = null) {
+        new RenderNode(renderObject: Optional.ofNullable(obj), renderOptions: Optional.ofNullable(options), childNodes: childNodes)
     }
 
     static RenderNode leaf(Renderable obj, RenderOptions options = RenderOptions.empty) {
-        new RenderNode(renderObject: obj, renderOptions: options, childNodes: [])
+        new RenderNode(renderObject: Optional.of(obj), renderOptions: Optional.of(options), childNodes: [])
     }
 }
--- render-demo-clock/src/main/groovy/gameObject/player/ClockRenderComponent.groovy
+++ render-demo-clock/src/main/groovy/gameObject/player/ClockRenderComponent.groovy
@@ -12,61 +12,71 @@ import renderer.shape.Point
 
 class ClockRenderComponent extends RenderComponent {
 
-    private List<RenderNode> secondMarks
-    private List<RenderNode> minuteMarks
-    private List<RenderNode> hourMarks
+    private RenderNode minuteMarks
+    private RenderNode hourMarks
 
     @Override
     RenderNode getRenderNode() {
         Clock parent = (Clock) parent
         // Clock marks.
-        def nodes = [
-                // Clock circle.
-                RenderNode.leaf(new Circle(center: parent.center, radius: parent.radius), new RenderOptions(drawColor: DrawColor.BLACK)),
-                // Handles.
-                RenderNode.leaf(new Line(parent.center, parent.secondHandlePos), new RenderOptions(drawColor: DrawColor.RED)),
-                RenderNode.leaf(new Line(parent.center, parent.minuteHandlePos), new RenderOptions(drawColor: DrawColor.BLUE)),
-                RenderNode.leaf(new Line(parent.center, parent.hourHandlePos), new RenderOptions(drawColor: DrawColor.GREEN))
-        ]
+        def nodes = []
 
-        def hourMarks = getHourMarks()
-        def minuteMarks = getMinuteMarks()
-        nodes = nodes + hourMarks + minuteMarks
+        nodes << getClockCircle()
+        nodes << getHandles()
+        nodes << getHourMarks()
+        nodes << getMinuteMarks()
+
         return RenderNode.node(
+                nodes,
                 // Center.
                 new Point(pos: parent.center),
                 // TODO: Make marks gray.
                 new RenderOptions(drawColor: DrawColor.BLACK),
-                // TODO: Rework tree structure - marks should be at bottom.
-                nodes
         )
     }
 
-    private getMinuteMarks() {
+    private getClockCircle() {
+        Clock parent = (Clock) parent
+        RenderNode.leaf(new Circle(center: parent.center, radius: parent.radius), new RenderOptions(drawColor: DrawColor.BLACK))
+    }
+
+    private RenderNode getHandles() {
+        Clock parent = (Clock) parent
+        def handles = [
+                RenderNode.leaf(new Line(parent.center, parent.secondHandlePos), new RenderOptions(drawColor: DrawColor.RED)),
+                RenderNode.leaf(new Line(parent.center, parent.minuteHandlePos), new RenderOptions(drawColor: DrawColor.BLUE)),
+                RenderNode.leaf(new Line(parent.center, parent.hourHandlePos), new RenderOptions(drawColor: DrawColor.GREEN))
+        ]
+        return RenderNode.node(handles)
+    }
+
+    private RenderNode getMinuteMarks() {
         if (minuteMarks) {
             return minuteMarks
         }
         Clock clock = (Clock) parent
-        minuteMarks = (1..60).collect {
+        def minuteMarkNodes = (1..60).collect {
             def minuteStep = clock.MINUTE_CIRCLE_STEP * it
             def minutePosOnCircle = CircleOperations.getPointOnCircleInRadians(clock.circleDesc, minuteStep)
             def line = new Line(minutePosOnCircle + (clock.center - minutePosOnCircle) * 0.05, minutePosOnCircle)
             RenderNode.leaf(line, new RenderOptions(drawColor: DrawColor.YELLOW))
         }
-        return hourMarks
+        minuteMarks = RenderNode.node(minuteMarkNodes)
+        minuteMarks
     }
 
-    private getHourMarks() {
+    private RenderNode getHourMarks() {
         if (hourMarks) {
             return hourMarks
         }
         Clock clock = (Clock) parent
-        hourMarks = (1..12).collect {
+        def hourMarkNodes = (1..12).collect {
             def hourStep = clock.HOUR_CIRCLE_STEP * it
             def hourPosOnCircle = CircleOperations.getPointOnCircleInRadians(clock.circleDesc, hourStep)
             def line = new Line(hourPosOnCircle + (clock.center - hourPosOnCircle) * 0.2, hourPosOnCircle)
             RenderNode.leaf(line, new RenderOptions(drawColor: DrawColor.YELLOW))
         }
-        return hourMarks
+        hourMarks = RenderNode.node(hourMarkNodes)
+        hourMarks
     }
 }
