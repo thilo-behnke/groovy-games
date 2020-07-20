2ab95e336383de5ea1ebb43e684cf46e87e01e7a
Thilo Behnke <thilo.behnke@gmx.net>
2020-07-20 21:09:54 +0200

    broken test for empty node scenario

---
+++ core/src/test/groovy/gameObject/components/RenderComponentSpec.groovy
@@ -0,0 +1,34 @@
+package gameObject.components
+
+import renderer.destination.RenderDestination
+import renderer.renderObjects.RenderNode
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@Unroll
+class RenderComponentSpec extends Specification {
+    class DummyRenderComponent extends RenderComponent {
+        RenderNode node
+
+        @Override
+        RenderNode getRenderNode() {
+            return node
+        }
+    }
+
+    RenderDestination renderDestinationMock
+
+    def setup() {
+        renderDestinationMock = Mock(RenderDestination)
+    }
+
+    def 'should do nothing if the renderNode is empty and has no children'() {
+        given:
+        def renderComp = new DummyRenderComponent(node: RenderNode.node([]))
+        when:
+        renderComp.render(renderDestinationMock)
+        then:
+        // TODO: Fix.
+        1 * renderComp.performRender(renderComp.renderNode, renderDestinationMock)
+    }
+}
