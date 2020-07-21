32607e2764e999b984e5d9cde2bde4514687ed87
Thilo Behnke <thilo.behnke@gmx.net>
2020-07-21 22:19:46 +0200

    fix build

--- demo-input-bindings/src/main/groovy/EntryPoint.groovy
+++ demo-input-bindings/src/main/groovy/EntryPoint.groovy
@@ -1,15 +1,11 @@
 import engine.DefaultGameScene
 import engine.GameEngine
 import gameObject.GameObjectProvider
-import gameObject.player.Clock
 
 GameEngine gameEngine = GameEngineProvider.provideGameEngine()
 
 def gameObjectProvider = new GameObjectProvider()
 
-def player = Clock.create()
-
-gameObjectProvider << player
 def defaultScene = new DefaultGameScene('default', gameObjectProvider)
 gameEngine.addScene(defaultScene)
 gameEngine.changeScene(defaultScene.name)
