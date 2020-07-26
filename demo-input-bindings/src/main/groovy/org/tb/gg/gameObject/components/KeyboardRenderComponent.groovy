bd1321f0659a665712cd4590befafd6faaeb084f
Thilo Behnke <thilo.behnke@gmx.net>
2020-07-26 18:58:14 +0200

    add title to keyboard example

--- demo-input-bindings/src/main/groovy/org/tb/gg/gameObject/components/KeyboardRenderComponent.groovy
+++ demo-input-bindings/src/main/groovy/org/tb/gg/gameObject/components/KeyboardRenderComponent.groovy
@@ -9,14 +9,38 @@ import org.tb.gg.renderer.options.RenderOptions
 import org.tb.gg.renderer.renderObjects.RenderNode
 import org.tb.gg.renderer.shape.Text
 
+import java.awt.Rectangle
+
 // TODO: Add Keyboard visualization to show pressed keys.
 @Log4j
 class KeyboardRenderComponent extends RenderComponent {
+
+    private final static TITLE_POS_Y = -10
+    private final static ACTIVE_ACTIONS_POS_Y = -30
+
     Vector pos
 
     @Override
     RenderNode getRenderNode() {
+        RenderNode.node([
+                createTitleText(),
+                createActiveActionsText()
+        ])
+    }
+
+    // TODO: Creat rectangle, properly position text...
+    private createTitleText() {
         def keyboard = (Keyboard) parent
-        return RenderNode.leaf(new Text(pos: pos, text: keyboard.activeActions.join(', ')), new RenderOptions(drawColor: DrawColor.BLACK))
+        RenderNode.leaf(
+                new Text(pos: pos + new Vector(x: 0, y: TITLE_POS_Y), text: "Keyboard: ${keyboard.id}"),
+                new RenderOptions(drawColor: DrawColor.BLACK)
+        )
+    }
+
+    private createActiveActionsText() {
+        def keyboard = (Keyboard) parent
+        RenderNode.leaf(
+                new Text(pos: pos + new Vector(x: 0, y: ACTIVE_ACTIONS_POS_Y), text: keyboard.activeActions.join(', ')), new RenderOptions(drawColor: DrawColor.BLACK)
+        )
     }
 }
