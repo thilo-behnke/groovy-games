83ce4bb3d0b300c5e5deda5331d71eaebe868461
Thilo Behnke <thilo.behnke@gmx.net>
2020-07-28 22:07:21 +0200

    refactore order resolver

--- core/src/main/groovy/org/tb/gg/di/SinglePipelineServiceCreationOrderResolver.groovy
+++ core/src/main/groovy/org/tb/gg/di/SinglePipelineServiceCreationOrderResolver.groovy
@@ -1,23 +1,35 @@
 package org.tb.gg.di
 
-import org.tb.gg.di.Injected
-import org.tb.gg.di.ServiceCreationOrderResolver
 import org.tb.gg.di.definition.Service
 
 class SinglePipelineServiceCreationOrderResolver implements ServiceCreationOrderResolver {
     List<List<Class<? extends Service>>> determineCreationOrder(Set<Class<? extends Service>> serviceClasses) {
-        Map<Class<? extends Service>, List<Class<? extends Service>>> dependenciesPerService = serviceClasses.collectEntries {
+
+        Map<Class<? extends Service>, List<Class<? extends Service>>> servicesWithDependencies = identifyServiceDependencies(serviceClasses)
+        def pipe = createOrderedPipelineBasedOnDependencies(servicesWithDependencies)
+
+        return [pipe]
+    }
+
+    private static Map<Class<? extends Service>, List<Class<? extends Service>>> identifyServiceDependencies(Set<Class<? extends Service>> serviceClasses) {
+        serviceClasses.collectEntries {
             def methods = new ArrayList<>(Arrays.asList(it.getDeclaredMethods()))
             def injectedSetters = methods.findAll { method -> method.isAnnotationPresent(Injected.class) }
             def injectedServices = injectedSetters.collect { setter -> setter.returnType }
-            def undeclaredInjectedServices = injectedServices.findAll { injectedService -> !serviceClasses.find { service -> service.getTypeName() == injectedService.getTypeName() } }
+            def undeclaredInjectedServices = identifyUndeclaredServices(injectedServices, serviceClasses)
             if (undeclaredInjectedServices.size() > 0) {
                 throw new IllegalStateException("Found injected property that is itself not declared as a service: ${undeclaredInjectedServices.collect { it.getName() }.join(', ')}")
             }
             [(it): injectedServices]
         }
-        // Simple solution for now - order by number of dependencies.
-        def orderedServices = dependenciesPerService
+    }
+
+    private static identifyUndeclaredServices(List<Class<? extends Service>> injectedServices, Set<Class<? extends Service>> allServiceClasses) {
+        injectedServices.findAll { injectedService -> !allServiceClasses.find { service -> service.getTypeName() == injectedService.getTypeName() } }
+    }
+
+    private static createOrderedPipelineBasedOnDependencies(Map<Class<? extends Service>, List<Class<? extends Service>>> servicesWithDependencies) {
+        def orderedServices = servicesWithDependencies
                 .sort { a, b -> a.getValue().size() <=> b.getValue().size() }
 
         def pipe = new ArrayList<Class<Service>>()
@@ -37,7 +49,6 @@ class SinglePipelineServiceCreationOrderResolver implements ServiceCreationOrder
             pipe.add(serviceClass)
         }
 
-        pipe = pipe.unique()
-        return [pipe]
+        pipe.unique()
     }
 }
