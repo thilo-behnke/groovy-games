f14baf3c7f66216eec63ab4166ce4f8d8f1a2647
Thilo Behnke <thilo.behnke@gmx.net>
2020-08-02 18:56:13 +0200

    move circle with wasd

--- demo-shape-collisions/src/main/groovy/org/tb/gg/gameObject/CircleGameObject.groovy
+++ demo-shape-collisions/src/main/groovy/org/tb/gg/gameObject/CircleGameObject.groovy
@@ -1,6 +1,7 @@
 package org.tb.gg.gameObject
 
 import org.tb.gg.gameObject.GameObject
+import org.tb.gg.gameObject.component.MovableCircleAction
 import org.tb.gg.global.geom.Vector
 
 class CircleGameObject extends GameObject {
@@ -9,7 +10,21 @@ class CircleGameObject extends GameObject {
 
     @Override
     void update(Long timestamp, Long delta) {
-        def activeActions = inputComponent.getActiveActions()
-        System.println(activeActions)
+        def activeActions = inputComponent.getActiveActions().collect { MovableCircleAction.valueOf(it) }
+        def newX = center.x
+        def newY = center.y
+        // Update X.
+        if (activeActions.contains(MovableCircleAction.RIGHT)) {
+            newX = newX + 1 * delta
+        } else if (activeActions.contains(MovableCircleAction.LEFT)) {
+            newX = newX - 1 * delta
+        }
+        // Update Y.
+        if (activeActions.contains(MovableCircleAction.UP)) {
+            newY = newY + 1 * delta
+        } else if (activeActions.contains(MovableCircleAction.DOWN)) {
+            newY = newY - 1 * delta
+        }
+        center = new Vector(x: newX, y: newY)
     }
 }
