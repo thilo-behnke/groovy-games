a3222f2b70ea8d11436a2d7263819ba0f4124539
Thilo Behnke <thilo.behnke@gmx.net>
2020-08-04 20:03:14 +0200

    write tests for line.setCenter

--- core/src/test/groovy/org/tb/gg/renderer/shape/LineSpec.groovy
+++ core/src/test/groovy/org/tb/gg/renderer/shape/LineSpec.groovy
@@ -15,7 +15,7 @@ class LineSpec extends Specification {
     }
 
     @Unroll
-    def 'should identify points to be outside of the line'() {
+    def 'isPointWithin: should identify points to be outside of the line'() {
         expect:
         !line.isPointWithin(point)
         where:
@@ -31,16 +31,28 @@ class LineSpec extends Specification {
     }
 
     @Unroll
-    def 'should identify points to be on the line'() {
+    def 'isPointWithin: should identify points to be on the line'() {
         expect:
         line.isPointWithin(point)
         where:
         point                      | _
         new Vector(x: 0, y: 0)     | _
-        // TODO: Broken, because normalize does not round... Maybe just round x and y when a vector is created?
         new Vector(x: 2, y: 2.0)   | _
         new Vector(x: 2.0, y: 2.0) | _
         new Vector(x: 3.0, y: 3.0) | _
         new Vector(x: 4.0, y: 4.0) | _
+    }
+
+    @Unroll
+    def 'setCenter'() {
+        expect:
+        line.setCenter(newCenter)
+        line.start == start
+        line.end == end
+        where:
+        newCenter                 || start                     | end
+        new Vector(x: 0, y: 0)    || new Vector(x: -2, y: -2)  | new Vector(x: 2, y: 2)
+        new Vector(x: 4, y: 4)    || new Vector(x: 2, y: 2)    | new Vector(x: 6, y: 6)
+        new Vector(x: 883, y: 12) || new Vector(x: 881, y: 10) | new Vector(x: 885, y: 14)
     }
 }
