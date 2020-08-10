603fbea4a0ad025ecb51e73be7e8db3f7203c42a
Thilo Behnke <thilo.behnke@gmx.net>
2020-08-10 23:07:59 +0200

    working on rotation of player

--- game-shooter/src/main/groovy/org/tb/gg/gameObject/component/PlayerGameObject.groovy
+++ game-shooter/src/main/groovy/org/tb/gg/gameObject/component/PlayerGameObject.groovy
@@ -3,8 +3,10 @@ package org.tb.gg.gameObject.component
 import org.tb.gg.gameObject.GameObject
 import org.tb.gg.gameObject.components.physics.ShapeBody
 import org.tb.gg.gameObject.factory.KeyBoundGameObjectBuilder
+import org.tb.gg.gameObject.shape.Circle
 import org.tb.gg.gameObject.shape.Line
 import org.tb.gg.gameObject.shape.Rect
+import org.tb.gg.global.geom.CircleOperations
 import org.tb.gg.global.geom.Vector
 import org.tb.gg.global.math.MathConstants
 
@@ -34,6 +36,7 @@ class PlayerGameObject extends GameObject {
         }
 
         updateMovement(activeActions, timestamp, delta)
+        updateOrientation(activeActions, timestamp, delta)
     }
 
     private updateMovement(List<PlayerAction> activeActions, Long timestamp, Long delta) {
@@ -58,6 +61,38 @@ class PlayerGameObject extends GameObject {
 
     private updateOrientation(List<PlayerAction> activeActions, Long timestamp, Long delta) {
         // TODO: Update orientation
+        def goal = null
 
+        if (activeActions.containsAll([PlayerAction.LOOK_UP, PlayerAction.LOOK_RIGHT])) {
+            goal = new Vector(x: 1, y: 1)
+        } else if (activeActions.containsAll([PlayerAction.LOOK_RIGHT, PlayerAction.LOOK_DOWN])) {
+            goal = new Vector(x: 1, y: -1)
+        } else if (activeActions.containsAll([PlayerAction.LOOK_DOWN, PlayerAction.LOOK_LEFT])) {
+            goal = new Vector(x: -1, y: -1)
+        } else if (activeActions.containsAll([PlayerAction.LOOK_LEFT, PlayerAction.LOOK_UP])) {
+            goal = new Vector(x: -1, y: 1)
+        } else if (activeActions.contains(PlayerAction.LOOK_UP)) {
+            goal = new Vector(x: 0, y: 1)
+        } else if (activeActions.contains(PlayerAction.LOOK_RIGHT)) {
+            goal = new Vector(x: 1, y: 0)
+        } else if (activeActions.contains(PlayerAction.LOOK_DOWN)) {
+            goal = new Vector(x: 0, y: -1)
+        } else if (activeActions.contains(PlayerAction.LOOK_LEFT)) {
+            goal = new Vector(x: -1, y: 0)
+        }
+
+        if (goal == null) {
+            return
+        }
+
+        def angle1 = goal.angleBetween(orientation)
+        def angle2 = orientation.angleBetween(goal)
+
+        def shorterDirectionToGoal = angle1 >= angle2 ? orientation - goal : goal - orientation
+        def rotationDirection = shorterDirectionToGoal.dot(orientation.rotate(MathConstants.HALF_PI)) >= 0 ? 1 : -1
+
+        orientation = orientation.rotate(1000.0 / 60.0 * rotationDirection)
+
+        return
     }
 }
