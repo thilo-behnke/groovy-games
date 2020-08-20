161211b6bf36a8ed907772a1ee1548e9d1a48b55
Thilo Behnke <thilo.behnke@gmx.net>
2020-08-20 23:07:23 +0200

    refactoring

--- core/src/main/groovy/org/tb/gg/di/creator/DefaultConstructorServiceCreator.groovy
+++ core/src/main/groovy/org/tb/gg/di/creator/DefaultConstructorServiceCreator.groovy
@@ -5,6 +5,7 @@ import org.tb.gg.di.ServiceProvider
 import org.tb.gg.di.config.ServiceMappingRegistry
 import org.tb.gg.di.definition.MultiInstanceService
 import org.tb.gg.di.definition.Service
+import org.tb.gg.di.definition.Singleton
 
 class DefaultConstructorServiceCreator implements ServiceCreator {
     private ServiceCreationOrderResolver serviceCreationOrderResolver
@@ -21,18 +22,12 @@ class DefaultConstructorServiceCreator implements ServiceCreator {
             // TODO: Could each be done in separate threads.
             pipe.collect { serviceClass ->
                 def serviceInstance = serviceClass.getConstructor().newInstance()
-                if (org.tb.gg.di.definition.Singleton.isAssignableFrom(serviceClass)) {
-                    def baseServiceClassName = getBaseClassNameIfServiceImplementation(serviceClass)
-                    if (baseServiceClassName) {
-                        ServiceProvider.registerSingletonService(serviceInstance, baseServiceClassName)
-                    } else {
-                        ServiceProvider.registerSingletonService(serviceInstance)
-                    }
+                if (Singleton.isAssignableFrom(serviceClass)) {
+                    registerSingleton((Singleton) serviceInstance)
                 } else if (MultiInstanceService.isAssignableFrom(serviceClass)) {
-                    // TODO: Not a great solution, but works for now...
-                    def multiInstanceInterface = serviceClass.getInterfaces().find { MultiInstanceService.isAssignableFrom(it) }
-                    System.out.println(multiInstanceInterface)
-                    ServiceProvider.registerMultiInstanceService(serviceInstance, multiInstanceInterface.getSimpleName())
+                    registerMultiInstanceService((MultiInstanceService) serviceInstance)
+                } else {
+                    throw new IllegalArgumentException("Unknown service implementation provided, don't know how to register: ${serviceClass}")
                 }
                 return serviceInstance
             }
@@ -41,5 +36,24 @@ class DefaultConstructorServiceCreator implements ServiceCreator {
 
     private getBaseClassNameIfServiceImplementation(Class<? extends Service> serviceClass) {
         return serviceMappingRegistry.getRegisteredServices().inverse().get(serviceClass)
+    }
+
+    private registerSingleton(Singleton singleton) {
+        def baseServiceClassName = getBaseClassNameIfServiceImplementation(singleton.class)
+        if (baseServiceClassName) {
+            ServiceProvider.registerSingletonService(singleton, baseServiceClassName)
+        } else {
+            ServiceProvider.registerSingletonService(singleton)
+        }
+    }
+
+    private registerMultiInstanceService(MultiInstanceService multiInstanceService) {
+        def multiInstanceInterface = multiInstanceService.class.getInterfaces()
+                .findAll { MultiInstanceService.isAssignableFrom(it) }
+                .collect { new Tuple2<Class<?>, Integer>(it, it.getInterfaces().size()) }
+                .sort { a, b -> a.getV2() <=> b.getV2() }
+                .collect { it.getV1() }
+                .first()
+        ServiceProvider.registerMultiInstanceService(multiInstanceService, multiInstanceInterface.getSimpleName())
     }
 }
