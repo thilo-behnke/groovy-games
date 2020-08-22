b72fc25665eb2d40e4c143ce285e0094e105bb33
Thilo Behnke <thilo.behnke@gmx.net>
2020-08-22 16:23:40 +0200

    add todo

--- core/src/main/groovy/org/tb/gg/collision/handler/GameObjectCollisionHandler.groovy
+++ core/src/main/groovy/org/tb/gg/collision/handler/GameObjectCollisionHandler.groovy
@@ -7,6 +7,7 @@ import org.tb.gg.gameObject.GameObject
 abstract class GameObjectCollisionHandler<S extends GameObject, T extends GameObject> implements CollisionHandler<S, T>, MultiInstanceService {
     abstract void handleCollision(S a, T b)
 
+    // TODO: Create with AST transformation.
     @Override
     boolean validForTypes(Object a, Object b) {
         return false
