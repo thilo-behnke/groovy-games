3ca5f024202b6dbb35c1b8b8620f0133fdfb5128
Thilo Behnke <thilo.behnke@gmx.net>
2020-08-23 11:55:58 +0200

    position score game object

--- game-shooter/src/main/groovy/org/tb/gg/gameObject/component/score/ScoreGameObject.groovy
+++ game-shooter/src/main/groovy/org/tb/gg/gameObject/component/score/ScoreGameObject.groovy
@@ -1,15 +1,31 @@
 package org.tb.gg.gameObject.component.score
 
+import org.tb.gg.config.ConfigurationService
+import org.tb.gg.config.ConfigurationSettings
+import org.tb.gg.di.Inject
 import org.tb.gg.gameObject.BaseGameObject
 import org.tb.gg.gameObject.components.physics.ShapeBody
 import org.tb.gg.gameObject.factory.GameObjectBuilder
 import org.tb.gg.gameObject.shape.Rect
 import org.tb.gg.global.geom.Vector
+import org.tb.gg.score.ScoreManager
 
 class ScoreGameObject extends BaseGameObject {
+    @Inject ScoreManager scoreManager
+    @Inject static ConfigurationService configurationService
+
+    int score
+
     static ScoreGameObject create() {
+        Vector scorePos = new Vector(x: configurationService.resolution.getV1() - 120, y: configurationService.resolution.getV2() - 20)
         (ScoreGameObject) new GameObjectBuilder<ScoreGameObject>(ScoreGameObject)
-            .setBody(new ShapeBody(new Rect(new Vector(x: 500, y: 500), new Vector(x: 100, y: 100))))
+            .setRenderComponent(new ScoreRenderComponent())
+            .setBody(new ShapeBody(new Rect(scorePos, new Vector(x: 100, y: 100))))
             .build()
+    }
+
+    @Override
+    void update(Long timestamp, Long delta) {
+        score = scoreManager.score
     }
 }
