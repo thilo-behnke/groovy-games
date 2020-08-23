f440a12bc5abf33eb6b5c3e614f69253cd9edad6
Thilo Behnke <thilo.behnke@gmx.net>
2020-08-23 15:05:38 +0200

    include player in solide collision handling? added todos

--- game-shooter/src/main/groovy/org/tb/gg/gameObject/component/player/PlayerPhysicsComponent.groovy
+++ game-shooter/src/main/groovy/org/tb/gg/gameObject/component/player/PlayerPhysicsComponent.groovy
@@ -28,4 +28,6 @@ class PlayerPhysicsComponent extends PhysicsComponent {
                 new PhysicStats(velocity: Vector.zeroVector())
         )
     }
+
+    // TODO: Instead of directly updating the player pos, use velocity and use a hook to also update the gun position, etc.
 }
